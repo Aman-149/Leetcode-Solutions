b655f89a9493f6b9bf02722d2699ae9479502a63
Aman-149 <amanuelfikadu23971@gmail.com>
2026-02-27 23:23:27 +0300

    Time: 3 ms (82.81%) | Memory: 20.5 MB (79.48%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        pointer=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[pointer]=nums[pointer],nums[i]
+                pointer=pointer+1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
